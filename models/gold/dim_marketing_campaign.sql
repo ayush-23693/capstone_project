a73d45a74a1ed523adf5dab61210ebdd571ee94c
Ayush Yadav <ayush.yadav@accordion.com>
2026-08-15 13:12:20 +0000

    added: marketing dimension

---
+++ models/gold/dim_marketing_campaign.sql
@@ -0,0 +1,14 @@
+{{ config(
+    materialized='table'
+) }}
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['CAMPAIGN_ID']) }} AS CAMPAIGN_KEY,
+    CAMPAIGN_ID,
+    TARGET_AUDIENCE_SEGMENT,
+    BUDGET,
+    CAMPAIGN_DURATION_DAYS AS DURATION,
+    ROI,
+    START_DATE,
+    END_DATE,
+FROM {{ ref('silver_marketing_campaign') }}
